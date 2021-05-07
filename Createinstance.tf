ba8e054a01eae363fd1c9d4b689c2670e491b976
ramkrishna2k3 <ramkrishna2k3@gmail.com>
2021-05-07 23:00:35 +0530

    added sg

--- Createinstance.tf
+++ Createinstance.tf
@@ -2,6 +2,22 @@ resource "aws_instance" "myfirstinstance" {
 ami       ="ami-0b9064170e32bde34"
 instance_type = "t2.micro"
 
+user_data = <<-EOF
+            #!/bin/bash
+            echo "Hello, World"> index.html
+            nohup busybox httpd -f -p 8080 &
+ 
+ resource   "aws_security_group" "instance"{ 
+     name = "multipleinstance"
+
+     ingress {
+         from_port     = 8080
+         to_port       = 8080
+         protocol      = "tcp"
+         cidr_blocks   = ["0.0.0.0/0"]
+     } 
+ }
+
 tags={
  Name = "multipleinstance"
    }
